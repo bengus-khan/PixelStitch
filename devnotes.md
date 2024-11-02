aa697bfd883c74048b404da5d18d5416994455b1
Benjamin Gus <116837159+bengus-khan@users.noreply.github.com>
2024-11-02 14:16:00 -0400

    Updated script and added backgrounds directory, pip requirements, and devnotes.md

---
+++ devnotes.md
@@ -0,0 +1,30 @@
+# Development notes
+
+## The 'eightstitch' directory
+This directory is the home of all python scripts for the project.
+
+## The 'backgrounds' directory
+This directory is the home of all background images and vector masks. Each supported grid aspect ratio will have a dedicated subdirectory within this directory (e.g., "/8-Stitch/backgrounds/16x16/"). Example directory structure shown below:
+
+```
+backgrounds/
+├── 128x128/
+│   ├── 128x128-1_raster.png
+│   ├── 128x128-1_mask.png
+│   ├── 128x128-2_raster.png
+│   └── 128x128-2_mask.svg
+├── 16x16/
+│   ├── 16x16-1_raster.png
+│   ├── 16x16-1_mask.png
+│   ├── 16x16-2_raster.png
+│   └── 16x16-2_mask.svg
+├── 32x32/
+│   ├── 32x32-1_raster.png
+│   ├── 32x32-1_mask.png
+│   ├── 32x32-2_raster.png
+│   └── 32x32-2_mask.svg
+├── backgrounds.json
+└── backgrounds.yaml
+```
+
+I'm still deciding between using `yaml` vs `json` for the background info file.
--- eightstitch/eightstitch.py
+++ eightstitch/eightstitch.py
@@ -1,40 +1,60 @@
 #!/usr/local/bin/python3
 
+# pare down these imports later, once you've decided which to actually use
 import argparse
+import cairosvg
+import json
 import logging
+import lxml
+import numpy
 import os
+import PIL
+import PIL.Image as Image
 import subprocess
+import svgpathtools
 import threading
+import xml
+import xml.etree.ElementTree as XmlTree
+import yaml
 
 '''
 DEVNOTES:
 - Some really meaningful notes, yeah!
 - Pixel aspect ratio needs to always be 1:1
 - Processing required:
+    - Parse background raster image
+        - Number of pixels & aspect ratio
     - Parse vector mask
         - Number of 'pixels' & aspect ratio
+        - Store contents of mask file as data
     - Parse design
         - Number of pixels & aspect ratio
         - If pixel count & aspect ratio match mask, store pixel color data (gotta figure out format)
-    - Generate design vector file
-        - Load contents of mask file as data
+    - Generate color mask
         - Apply design color data to mask data
         - Create new file and write updated mask data to file
-    - Generate resulting final image
-        - Somehow need to merge design vector data with background image - figure out appropriate method
+    - Export final raster frame (for file render applications)
+        - Need to merge design vector data with background image - figure out appropriate method
 
 '''
 
+# Input files
 user_design = None
+# Eventually going to list all background raster & mask files in a json or yaml file
 raster_background = None
 vector_mask = None
 
-class PixelToMask:
+class EightStitch:
 
     def __init__(self, user_design, raster_background, vector_mask):
-        self.design = user_design
-        self.bg_image = raster_background
-        self.bg_mask = vector_mask
+        self.design = Image.open(user_design)
+        self.bg_image = Image.open(raster_background)
+        # Determine best method(s) for loading vector mask data from svg file
+        self.mask2xmltree = XmlTree(vector_mask)
+        self.mask2paths = svgpathtools.svg2paths(vector_mask)
+
+    def ParseBackground(self):
+        print('placeholder')
 
     def ParseMask(self):
         print('placeholder')
@@ -42,5 +62,8 @@ class PixelToMask:
     def ParseDesign(self):
         print('placeholder')
 
-    def GenerateMaskFile(self):
+    def GenerateColorMask(self):
+        print('placeholder')
+
+    def ExportRasterFrame(self):
         print('placeholder')
